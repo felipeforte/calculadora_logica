a10ea08d21f0f82d03dcf484696ac2244be2e691
Felipe Forte <felipeforte@protonmail.com>
2023-09-01 19:58:24 -0300

    mais métodos

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-const simbolos = ["~","^","v",">","<>"];
+const operadores = ["^","v",">","<>"];
+const letras = ["A","B","C","D","E","F","P","Q","R","S","p","q","r","s"];
 var formula = prompt("Escreva uma proposição");
 
-function validar(String f) {
-    
+function validar(formula) {
+    var parenteses = [];
+    var premissas = [];
+    var array = formula.split("");
+    for (let i=0;i<array.length;i++) {
+        for (let j=0;j<letras.length;j++) {
+            if (array[i] == letras[j]) {
+                if (!contains(premissas,letras[j])) {
+                    premissas.push(letras[j])
+                    console.log(premissas);
+                }
+            }
+            
+        }
+    }
 }
 
-console.log(proposicao)
+function contains(array, string) {
+    for (let i=0;i<array.length;i++) {
+        if (array[i] == string) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+console.log(validar(formula));
