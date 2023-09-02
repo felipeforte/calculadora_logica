e103986d50576a52f6368678b96e80c9829af10d
Felipe Forte <felipeforte@protonmail.com>
2023-09-02 09:20:34 -0300

    algoritmo mais robusto para isolar premissas

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ const letras = ["A","B","C","D","E","F","P","Q","R","S","p","q","r","s"];
 var formula = prompt("Escreva uma proposição");
 
 function validar(formula) {
+    console.log(`Formula: ${formula}`)
     var parenteses = [];
     var premissas = [];
     var array = formula.split("");
     var p_stack = 0;
-    var p_start = 0;
+    var p_start = [];
     for (let i=0;i<array.length;i++) {
         if (array[i] == "(") {
-            p_start = i+1;
+            p_start[p_stack] = i+1;
             p_stack++;
             // console.log(`p_stack = ${p_stack}, p_start = ${p_start}`)
         }
@@ -18,13 +19,13 @@ function validar(formula) {
             if (p_stack == 0) {
                 alert("Erro de sintaxe: Parenteses final sem correspondente inicial")
                 return;
-            } else if (p_start == i) {
+            } else if (p_start[p_stack-1] == i) {
                 alert("Erro de sintaxe: Parenteses vazio")
                 return;
             }
-            premissas.push(formula.substring(p_start,i))
-            p_stack--;
+            premissas.push(formula.substring(p_start[p_stack-1],i))
             console.log(premissas);
+            p_stack--;
         }
         for (let j=0;j<letras.length;j++) {
             if (array[i] == letras[j]) {
@@ -36,10 +37,6 @@ function validar(formula) {
             
         }
     }
-}
-
-function checkParenthesis(s) {
-
 }
 
 function contains(array, string) {
