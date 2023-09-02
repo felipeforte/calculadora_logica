76661c3f5745c851aefbb9262caa8510c83b7c54
Felipe Forte <felipeforte@protonmail.com>
2023-09-02 08:43:06 -0300

    isolador de premissas

--- script.js
+++ script.js
@@ -6,17 +6,40 @@ function validar(formula) {
     var parenteses = [];
     var premissas = [];
     var array = formula.split("");
+    var p_stack = 0;
+    var p_start = 0;
     for (let i=0;i<array.length;i++) {
+        if (array[i] == "(") {
+            p_start = i+1;
+            p_stack++;
+            // console.log(`p_stack = ${p_stack}, p_start = ${p_start}`)
+        }
+        if (array[i] == ")") {
+            if (p_stack == 0) {
+                alert("Erro de sintaxe: Parenteses final sem correspondente inicial")
+                return;
+            } else if (p_start == i) {
+                alert("Erro de sintaxe: Parenteses vazio")
+                return;
+            }
+            premissas.push(formula.substring(p_start,i))
+            p_stack--;
+            console.log(premissas);
+        }
         for (let j=0;j<letras.length;j++) {
             if (array[i] == letras[j]) {
                 if (!contains(premissas,letras[j])) {
-                    premissas.push(letras[j])
+                    premissas.push(letras[j]);
                     console.log(premissas);
                 }
             }
             
         }
     }
+}
+
+function checkParenthesis(s) {
+
 }
 
 function contains(array, string) {
